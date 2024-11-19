8549cd7aabb3ae41bb24fa122fea8b6bc38e003b
Alice <hamstenal.ex@gmail.com>
2024-11-19 13:11:33 +0530

    Create x.js

---
+++ x.js
@@ -0,0 +1,44 @@
+const Twitter = require('twitter-v2');
+const fs = require('fs');
+const axios = require('axios');
+
+// Fake Twitter API credentials
+const client = new Twitter({
+    bearer_token: 'YOUR_TWITTER_BEARER_TOKEN'
+});
+
+async function downloadImage(imageUrl, filePath) {
+    const writer = fs.createWriteStream(filePath);
+    const response = await axios.get(imageUrl, { responseType: 'stream' });
+    response.data.pipe(writer);
+    return new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+    });
+}
+
+async function postToTwitter(imageUrl, text) {
+    const imagePath = './meme.jpg';
+    
+    // Download the image
+    await downloadImage(imageUrl, imagePath);
+
+    // Read image file
+    const imageData = fs.readFileSync(imagePath);
+
+    // Fake Twitter media upload
+    console.log('Uploading image to Twitter...');
+    const mediaId = 'FAKE_MEDIA_ID'; // Replace with real Twitter API media ID
+    console.log(`Media uploaded with ID: ${mediaId}`);
+
+    // Post tweet
+    console.log('Posting tweet...');
+    return {
+        status: 'success',
+        tweet_id: 'FAKE_TWEET_ID',
+        text,
+        image_url: imageUrl,
+    };
+}
+
+module.exports = { postToTwitter };
